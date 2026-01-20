d7c6ef5b11edc75d9a532f862fb547dfb06f7c15
SriramKeerthi <shramk@gmail.com>
2026-01-20 14:36:43 +0100

    fix: Lint errors - remove unused imports and disable fast refresh warning
    - Remove unused SvgIcon import from SiteHeader.tsx
    - Remove unused Avatar import from About.tsx
    - Disable react-refresh warning in ThemeContext.tsx (acceptable for context files)

--- openlights-docs/src/contexts/ThemeContext.tsx
+++ openlights-docs/src/contexts/ThemeContext.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-refresh/only-export-components */
 import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react';
 import { ThemeProvider } from '@mui/material';
 import { theme, darkTheme } from '../theme';
